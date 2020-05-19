477a815c4f8de2bfbc1ed35269b000f2b17cc851
Viproz <viproz12@gmail.com>
2020-05-20 00:23:44 +0200

    Format and fixed typo.

--- foc/pssdrift.py
+++ foc/pssdrift.py
@@ -24,7 +24,8 @@ from scipy import signal
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
-scaler = MinMaxScaler(feature_range=(0, 1))
+scaler = MinMaxScaler(feature_range = (0, 1))
+
 
 # get_peaks: get PSS peaks jumping from the last position known that was a valid peak
 # Output:
@@ -46,7 +47,7 @@ def get_peaks (iq, pss_step, search_window, resample_factor, z_sequence, th_corr
 
         slice = iq[start_i:end_i]
 
-        #scaler = MinMaxScaler(feature_range=(0, 1))
+        # scaler = MinMaxScaler(feature_range=(0, 1))
 
         # Correlation between template and slice signal
         res_corr = abs(signal.correlate(slice, z_sequence))
@@ -55,20 +56,18 @@ def get_peaks (iq, pss_step, search_window, resample_factor, z_sequence, th_corr
         res_corr = scaler.transform(res_corr)
         # Trick to speed up the whole process.
         # Upsampling the correlaiton result (thanks Fabio!)
-        res_corr_up = signal.resample(res_corr, len(res_corr)*resample_factor)
+        res_corr_up = signal.resample(res_corr, len(res_corr) * resample_factor)
 
         # Find peaks in the correlation result
-        x = np.resize(res_corr_up,len(res_corr_up),)
-        peaks, _ = signal.find_peaks(x, distance=(len(slice)*resample_factor)-2, height=th_corr)
-
+        x = np.resize(res_corr_up, len(res_corr_up),)
+        peaks, _ = signal.find_peaks(x, distance = (len(slice) * resample_factor) - 2, height = th_corr)
 
         if (len(peaks) == 0):
-            #print("[WARNING] No peak detected, corr_factor=%.2f\n" % th_corr);
+            # print("[WARNING] No peak detected, corr_factor=%.2f\n" % th_corr);
             peak_list.append(None)
         else:
-            p = start_i + (peaks[0])/resample_factor -1;
+            p = start_i + (peaks[0]) / resample_factor - 1;
             peak_list.append(p)
-
 
         # No peak detected in this iteration -> No PSS detected
         # Move PSS_STEP forwards from start_i
@@ -81,13 +80,11 @@ def get_peaks (iq, pss_step, search_window, resample_factor, z_sequence, th_corr
             start_i = index_done + pss_step - search_window
             end_i = index_done + pss_step + search_window;
 
-
         if (debug_plot):
-            xnew = np.linspace(0, len(res_corr), len(res_corr)*resample_factor, endpoint=False)
-            plt.figure(figsize=(14,5))
-            plt.plot(xnew, res_corr_up, '.-', label="upsampled")
-            plt.plot(res_corr, 'ro-', label="original")
-
+            xnew = np.linspace(0, len(res_corr), len(res_corr) * resample_factor, endpoint = False)
+            plt.figure(figsize = (14, 5))
+            plt.plot(xnew, res_corr_up, '.-', label = "upsampled")
+            plt.plot(res_corr, 'ro-', label = "original")
 
     return np.array(peak_list)
 
@@ -97,69 +94,66 @@ def get_peaks (iq, pss_step, search_window, resample_factor, z_sequence, th_corr
 #    - pss_step: distance between PSS in I/Q samples
 #    - degree: degree of the polinomial regresion.
 
-def analyze_drift (peaks, pss_step, degree, debug_plot = False ):
 
-    pss_detected = peaks# - peaks[0]
-    x = np.array(list(range(0,len(peaks))))
-    index_nones=[i for i,v in enumerate(pss_detected) if v == None]
-    #index_nones=sorted(index_nones, reverse=True)
-    
+def analyze_drift (peaks, pss_step, degree, debug_plot = False):
+
+    pss_detected = peaks  # - peaks[0]
+    x = np.array(list(range(0, len(peaks))))
+    index_nones = [i for i, v in enumerate(pss_detected) if v == None]
+    # index_nones=sorted(index_nones, reverse=True)
+
     print(index_nones)
 
     x = np.delete(x, index_nones)
     pss_detected = np.delete(pss_detected, index_nones)
 
     pss_detected = pss_detected - pss_detected[0]
-    cumm_drift = pss_detected - x*pss_step
+    cumm_drift = pss_detected - x * pss_step
 
-    p = np.polyfit(x.tolist(),cumm_drift.tolist(),1)
-    y = np.polyval(p,x.tolist())
+    p = np.polyfit(x.tolist(), cumm_drift.tolist(), 1)
+    y = np.polyval(p, x.tolist())
 
-    PPM= ((y[-1]-y[0])/((x[-1]-x[0])*pss_step))*1e6;
-
+    PPM = ((y[-1] - y[0]) / ((x[-1] - x[0]) * pss_step)) * 1e6;
 
     if (debug_plot):
         t = pss_step / 1.92e6
-        plt.figure(figsize=(14,5))
-        plt.plot(x*t,cumm_drift, 'o', label='drift')
-        plt.plot(x*t,y, '.-' ,label='slope')
+        plt.figure(figsize = (14, 5))
+        plt.plot(x * t, cumm_drift, 'o', label = 'drift')
+        plt.plot(x * t, y, '.-' , label = 'slope')
         plt.xlabel('time (seconds)')
         plt.ylabel('cumm drift (IQ samples)')
-        plt.legend(fontsize=15)
+        plt.legend(fontsize = 15)
         plt.grid(True)
         plt.show()
-
 
     return PPM
 
 
-
-# Get proper Zadoof sequence
+# Get proper Zadoff sequence
 # Return:
 #   - lpeaks:
 #   - th_learned
-#   - sequence_zadoof
-def get_drift (iq, z_sequences, preamble, pss_step, search_window, resample_factor, fs, debug_plot=False):
+#   - sequence_zadoff
+def get_drift (iq, z_sequences, preamble, pss_step, search_window, resample_factor, fs, debug_plot = False):
 
-    #training_samples = len(iq)
+    # training_samples = len(iq)
     training_samples = pss_step * preamble
 
-    # Correlation with the zadoof templates over the training samples
-    corr=np.array([signal.correlate(iq[:training_samples], (z_sequences[0]))])
-    corr=np.append(corr, [np.array(signal.correlate(iq[:training_samples], (z_sequences[1])))],0)
-    corr=np.append(corr, [np.array(signal.correlate(iq[:training_samples], (z_sequences[2])))],0)
+    # Correlation with the zadoff templates over the training samples
+    corr = np.array([signal.correlate(iq[:training_samples], (z_sequences[0]))])
+    corr = np.append(corr, [np.array(signal.correlate(iq[:training_samples], (z_sequences[1])))], 0)
+    corr = np.append(corr, [np.array(signal.correlate(iq[:training_samples], (z_sequences[2])))], 0)
 
     # Normalize correlation
-    #scaler = MinMaxScaler(feature_range=(0, 1))
-
+    # scaler = MinMaxScaler(feature_range=(0, 1))
 
     data = ([np.reshape(abs(corr[0]), (-1, 1))])
-    data = np.append(data, [np.reshape(abs(corr[1]), (-1, 1))],0)
-    data = np.append(data, [np.reshape(abs(corr[2]), (-1, 1))],0)
+    data = np.append(data, [np.reshape(abs(corr[1]), (-1, 1))], 0)
+    data = np.append(data, [np.reshape(abs(corr[2]), (-1, 1))], 0)
 
-    scaler.fit_transform(np.concatenate( (data[0], data[1], data[2]), axis=0))
+    scaler.fit_transform(np.concatenate((data[0], data[1], data[2]), axis = 0))
 
-    for i in range(0,data.shape[0]):
+    for i in range(0, data.shape[0]):
         data[i] = scaler.transform(data[i])
 
     max_corr = 0.0
@@ -167,68 +161,64 @@ def get_drift (iq, z_sequences, preamble, pss_step, search_window, resample_fact
     l_peaks = None
     th_win = None
 
-    for i in range(0,data.shape[0]):
+    for i in range(0, data.shape[0]):
         # learn the threshold
 
         tmp_sorted = data[i][0:training_samples]
-        tmp_sorted = np.resize(tmp_sorted,(len(tmp_sorted),))
-        tmp_sorted=np.sort(tmp_sorted)[::-1]  # reverse order (descendent)
+        tmp_sorted = np.resize(tmp_sorted, (len(tmp_sorted),))
+        tmp_sorted = np.sort(tmp_sorted)[::-1]  # reverse order (descendent)
 
+        th_learned = (tmp_sorted[preamble] + tmp_sorted[preamble * 2 + 1]) / 2
+        th_learned = th_learned * 0.70
+        # print("th_learned[seq=%d]: %.2f" % (i,th_learned))
+        x = np.resize(data[i], len(data[i]),)
 
-        th_learned=(tmp_sorted[preamble] + tmp_sorted[preamble*2+1])/2
-        th_learned=th_learned*0.70
-        #print("th_learned[seq=%d]: %.2f" % (i,th_learned))
-        x = np.resize(data[i],len(data[i]),)
-
-        peaks, _ = signal.find_peaks(x[0:training_samples], distance=(pss_step-search_window), height=th_learned)
+        peaks, _ = signal.find_peaks(x[0:training_samples], distance = (pss_step - search_window), height = th_learned)
         p_value = max(x[peaks])
         if (p_value > max_corr):
             max_corr = p_value
             l_peaks = peaks;
             seq = i
             th_win = th_learned
-            
+
         if (debug_plot):
             plt.figure()
             plt.plot(x[0:training_samples])
             plt.plot(peaks, x[peaks], "x")
-            #plt.xlim(0, 50000)
+            # plt.xlim(0, 50000)
             plt.show()
 
-
     print("Winning sequence: " + str(seq))
-    if ( len ( np.where (abs(diff(peaks)-pss_step) > 10 )[0]) > 0 ):
+    if (len (np.where (abs(diff(peaks) - pss_step) > 10)[0]) > 0):
         print("[LTESSTRACK] Warning: Some PSS detected are further than %d +- 10 I/Q samples" % (pss_step))
 
-
     if (debug_plot):
-        x = np.resize(data[seq],len(data[seq]),)
+        x = np.resize(data[seq], len(data[seq]),)
         plt.figure()
         plt.plot(x[0:training_samples])
         plt.plot(l_peaks, x[l_peaks], "x")
-        #plt.xlim(0, 50000)
+        # plt.xlim(0, 50000)
         plt.show()
 
-
     # We assume peaks are properly located if they are in a range of 10 samples from the expected position
-    valid_peaks = np.where ((np.diff(l_peaks)-pss_step) < 10)
-    if ( len(valid_peaks)==0 or len(valid_peaks[0])==0 ):
+    valid_peaks = np.where ((np.diff(l_peaks) - pss_step) < 10)
+    if (len(valid_peaks) == 0 or len(valid_peaks[0]) == 0):
             print('[LTESSTRACK] Error: No valid PSS at the begining.')
             sys.exit(-1)
 
-    last_valid_peak = l_peaks[valid_peaks[0][-1]+1]
+    last_valid_peak = l_peaks[valid_peaks[0][-1] + 1]
     pss_detected = get_peaks(iq[last_valid_peak:], pss_step, search_window, resample_factor, z_sequences[seq], th_win, False)
 
     print(pss_detected)
     print(diff(pss_detected))
     # ideal pss detection in the given time
-    total_pss = int(len(iq)/pss_step) - int(last_valid_peak/pss_step)
-    index_nones=[i for i,v in enumerate(pss_detected) if v == None]
+    total_pss = int(len(iq) / pss_step) - int(last_valid_peak / pss_step)
+    index_nones = [i for i, v in enumerate(pss_detected) if v == None]
     nan_pss = len(index_nones)
-    confidence = (len(pss_detected) -nan_pss) / total_pss
+    confidence = (len(pss_detected) - nan_pss) / total_pss
 
     PPM = analyze_drift(pss_detected, pss_step, 1, debug_plot)
 
-    delta_f=(PPM*1e-6)*fs;
+    delta_f = (PPM * 1e-6) * fs;
 
     return PPM, delta_f, confidence
--- pyltessTrack.py
+++ pyltessTrack.py
@@ -29,44 +29,45 @@ from foc.pssdrift import get_drift
 import warnings
 from matplotlib.pyplot import psd, xlabel, ylabel, show
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action = 'ignore', category = FutureWarning)
 
- # Load Zadoof sequencies
-def get_zadoof_seqs (filename):
-    f = open(filename,'rb');
-    dat = np.fromfile(f, dtype=np.float32)
+
+# Load Zadoff sequencies
+def get_zadoff_seqs (filename):
+    f = open(filename, 'rb');
+    dat = np.fromfile(f, dtype = np.float32)
     dat = dat.astype(np.float32).view(np.complex64)
-    
+
     return dat
 
 
 # Constants
-VERSION="1.0-rc1"
+VERSION = "1.0-rc1"
 RESAMPLE_FACTOR = 20
 PSS_STEP = 9600
 SEARCH_WINDOW = 150
-PREAMBLE=30
+PREAMBLE = 30
 
 # variables
-fs=1.92e6
-fc=806e6
-chan=0
-gain=30
-source=-1
+fs = 1.92e6
+fc = 806e6
+chan = 0
+gain = 30
+source = -1
 
-AUX_BUFFER_SIZE = 20*1024
+AUX_BUFFER_SIZE = 20 * 1024
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("-s", '--source', type=int, dest='source', help="Set default SDR source device to use", default=source)
-    parser.add_argument("-f", '--frequency', type=int, dest='frequency',  help="Set LTE center frequency of the channel (Hz)", default=fc)
-    parser.add_argument("-g", '--gain', type=int, dest='gain',  help="Gain", default=gain)
-    parser.add_argument("-t", "--time", type=int, dest='time', help="Seconds collecting data on LTE frequency", default=1)
-    parser.add_argument("-j", '--json-file', dest='json', type=str,  help="Set the json file where results will be written", default=None)
-    parser.add_argument("-i", '--input-file', dest='inFile', type=str,  help="Use existing IQ file", default=None)
-    parser.add_argument("-d", '--debug', dest='debug',  help="enable debug mode with plots", action='store_true', default=False)
+    parser.add_argument("-s", '--source', type = int, dest = 'source', help = "Set default SDR source device to use", default = source)
+    parser.add_argument("-f", '--frequency', type = int, dest = 'frequency', help = "Set LTE center frequency of the channel (Hz)", default = fc)
+    parser.add_argument("-g", '--gain', type = int, dest = 'gain', help = "Gain", default = gain)
+    parser.add_argument("-t", "--time", type = int, dest = 'time', help = "Seconds collecting data on LTE frequency", default = 1)
+    parser.add_argument("-j", '--json-file', dest = 'json', type = str, help = "Set the json file where results will be written", default = None)
+    parser.add_argument("-i", '--input-file', dest = 'inFile', type = str, help = "Use existing IQ file", default = None)
+    parser.add_argument("-d", '--debug', dest = 'debug', help = "enable debug mode with plots", action = 'store_true', default = False)
     args = parser.parse_args()
 
     print("#########################################")
@@ -84,35 +85,33 @@ if __name__ == "__main__":
 
     # Look at for SDR devices
 #     sdr_list = SoapySDR.Device.enumerate()
-    index=0
+    index = 0
 
     # Set SDR and read samples
 
-    TOTAL_BUFFER_SIZE = int(fs*args.time)
+    TOTAL_BUFFER_SIZE = int(fs * args.time)
 
     print("[LTESSTRACK] Reading for %d seconds at %d MHz with gain=%d ... " % (args.time, args.frequency, args.gain))
     acq_time = datetime.datetime.now()
-    
-    
+
     data = []
-    
-    if args.inFile is None: # Get fresh data
+
+    if args.inFile is None:  # Get fresh data
         # This is unsafe, use rtlsdr wrapper instead
         command = "rtl_sdr -s " + str(fs) + " -f " + str(fc) + " -g 20 -n " + str(TOTAL_BUFFER_SIZE) + " -"
         print("Collecting data with the command: " + command)
         with subprocess.Popen(command, stdout = subprocess.PIPE, shell = True) as proc:
             data = proc.stdout.read()  # Read all at once
-    else: # Read the file given
+    else:  # Read the file given
         with open(args.inFile, "rb") as iqFile:
             data = iqFile.read(TOTAL_BUFFER_SIZE)
-            
 
     # Convert the sample to float complex values
     samples = np.frombuffer(data, dtype = np.uint8).astype(np.float32).view(np.complex64)
 
     if (args.debug):
         # use matplotlib to estimate and plot the PSD
-        psd(samples, NFFT=1024, Fs=fs/1e6, Fc=fc/1e6)
+        psd(samples, NFFT = 1024, Fs = fs / 1e6, Fc = fc / 1e6)
         xlabel('Frequency (MHz)')
         ylabel('Relative power (dB)')
         show()
@@ -121,30 +120,30 @@ if __name__ == "__main__":
 
     # load zadoof sequences (in time)
     try:
-        Z_sequences = np.array([get_zadoof_seqs("lte/25-Zadoff.bin"), \
-        get_zadoof_seqs("lte/29-Zadoff.bin"),\
-        get_zadoof_seqs("lte/34-Zadoff.bin")])
+        Z_sequences = np.array([get_zadoff_seqs("lte/25-Zadoff.bin"), \
+        get_zadoff_seqs("lte/29-Zadoff.bin"), \
+        get_zadoff_seqs("lte/34-Zadoff.bin")])
     except FileNotFoundError:
-        Z_sequences = np.array([get_zadoof_seqs("/usr/share/pyltesstrack/lte/25-Zadoff.bin"), \
-        get_zadoof_seqs("/usr/share/pyltesstrack/lte/29-Zadoff.bin"),\
-        get_zadoof_seqs("/usr/share/pyltesstrack/lte/34-Zadoff.bin")])
+        Z_sequences = np.array([get_zadoff_seqs("/usr/share/pyltesstrack/lte/25-Zadoff.bin"), \
+        get_zadoff_seqs("/usr/share/pyltesstrack/lte/29-Zadoff.bin"), \
+        get_zadoff_seqs("/usr/share/pyltesstrack/lte/34-Zadoff.bin")])
 
     # Get drift by analyzing the PSS time of arrival
-    [PPM, delta_f, confidence] = get_drift(samples, Z_sequences, PREAMBLE, PSS_STEP, SEARCH_WINDOW, RESAMPLE_FACTOR, fs, debug_plot=args.debug)
+    [PPM, delta_f, confidence] = get_drift(samples, Z_sequences, PREAMBLE, PSS_STEP, SEARCH_WINDOW, RESAMPLE_FACTOR, fs, debug_plot = args.debug)
 
-    print("[LTESSTRACK] Local oscilator error: %.8f PPM - [%.2f Hz], confidence=%.3f" % (PPM,delta_f,confidence))
+    print("[LTESSTRACK] Local oscilator error: %.8f PPM - [%.2f Hz], confidence=%.3f" % (PPM, delta_f, confidence))
 
     if (args.json):
-        data={}
-        data['datetime']=str(acq_time)
-        data['fc']=args.frequency
-        data['fs']=int(fs)
-        data['gain']=args.gain
-        data['sampling_time']=args.time
-        data['ppm']=PPM
-        data['confidence']=confidence
+        data = {}
+        data['datetime'] = str(acq_time)
+        data['fc'] = args.frequency
+        data['fs'] = int(fs)
+        data['gain'] = args.gain
+        data['sampling_time'] = args.time
+        data['ppm'] = PPM
+        data['confidence'] = confidence
 
-        with open(args.json, 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
+        with open(args.json, 'w', encoding = 'utf-8') as f:
+            json.dump(data, f, ensure_ascii = False, indent = 4)
 
         print("[LTESSTRACK] Results saved in " + args.json)
